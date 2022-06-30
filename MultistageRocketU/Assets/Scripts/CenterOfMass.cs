6c291b8b75e2d93456681dca8de82399fa2d79e1
xincas <ismrah.25@gmail.com>
2022-06-30 23:17:31 +0300

    draw vector of velocity

--- MultistageRocketU/Assets/Scripts/CenterOfMass.cs
+++ MultistageRocketU/Assets/Scripts/CenterOfMass.cs
@@ -1,13 +1,22 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using util;
 
 public class CenterOfMass : MonoBehaviour
 {
+    [SerializeField] private Vector2 maxMinVelocity;
     private void OnDrawGizmos()
     {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(GetComponent<Rigidbody>().worldCenterOfMass, 0.1f);
+        Gizmos.DrawSphere(rigidbody.worldCenterOfMass, 0.1f);
+        
+        Gizmos.color = Color.black;
+        Vector3 worldCenterOfMass = rigidbody.worldCenterOfMass;
+        Vector3 velocityDirection = rigidbody.GetPointVelocity(worldCenterOfMass);
+        float distance = Vector3.Distance(worldCenterOfMass, velocityDirection + worldCenterOfMass);
+        
+        DrawVector.ForGizmo(worldCenterOfMass, velocityDirection.normalized * Math.Clamp(distance, maxMinVelocity.x, maxMinVelocity.y));
     }
 }
