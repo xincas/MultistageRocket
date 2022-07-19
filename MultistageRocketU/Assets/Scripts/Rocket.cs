d4901f4b316c322cd4889973dd33e15649ebbfc9
xincas <ismrah.25@gmail.com>
2022-07-19 04:18:54 +0300

    adding Logger and fix collider calculation

--- MultistageRocketU/Assets/Scripts/Rocket.cs
+++ MultistageRocketU/Assets/Scripts/Rocket.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using util;
+using Logger = util.Logger;
 
 public class Rocket : MonoBehaviour
 {
@@ -18,13 +19,17 @@ public class Rocket : MonoBehaviour
     private ConstantForce _force;
     private CapsuleCollider _collider;
     private Transform _flameTransform;
+    private Transform _headTransform;
     private bool _isLaunched = false;
+
+    private Logger _logger = new Logger();
     void Start()
     {
         _rigidbody = transform.GetComponent<Rigidbody>();
         _force = transform.GetComponent<ConstantForce>();
         _collider = transform.GetComponent<CapsuleCollider>();
         _flameTransform = transform.Find("Flame");
+        _headTransform = transform.Find("Fairing");
         InitStages();
     }
 
@@ -39,11 +44,11 @@ public class Rocket : MonoBehaviour
             {
                 stage.FuelBurn(Time.fixedDeltaTime);
                 _rigidbody.mass -= stage.fuelСonsumption * Time.fixedDeltaTime;
-                _force.relativeForce = new Vector3(0f, stage.fuelСonsumption * stage.fuelVelocity, 0f);
+                _force.force = new Vector3(0f, stage.fuelСonsumption * stage.fuelVelocity, 0f);
             }
             else
             {
-                _force.relativeForce = Vector3.zero;
+                _force.force = Vector3.zero;
                 if (!stages.Remove(stage))
                 {
                     Debug.LogWarning("Can't delete last stage");
@@ -61,8 +66,7 @@ public class Rocket : MonoBehaviour
         }
         else
         {
-            _force.relativeForce = Vector3.zero;
-            Time.timeScale = 0f;
+            _force.force = Vector3.zero;
             Debug.Log("Max speed: " + Vector3.Distance(Vector3.zero, _rigidbody.velocity) + "m/s");
         }
 
@@ -81,7 +85,7 @@ public class Rocket : MonoBehaviour
         
         stTransform.SetParent(null);
         
-        BoxCollider stCollider = stGameObject.GetComponent<BoxCollider>();
+        CapsuleCollider stCollider = stGameObject.GetComponent<CapsuleCollider>();
         Rigidbody stRigidbody = stGameObject.GetComponent<Rigidbody>();
 
         stCollider.enabled = true;
@@ -95,20 +99,25 @@ public class Rocket : MonoBehaviour
         if (stagePrefab is null)
             return;
 
-        Transform lastTransform = null;
+        Transform lastTransform = _headTransform;
         Vector3 localScale = transform.localScale;
         
-        for (int i = 0; i < stages.Count; ++i)
+        Vector3 pos = - new Vector3(0f, 2.2f, 0);
+        GameObject nStage = Instantiate(stagePrefab, Vector3.zero, lastTransform.rotation);
+        GameObject interStage = nStage.transform.GetChild(0).GetChild(3).gameObject;
+        interStage.SetActive(false);
+        nStage.transform.SetParent(transform);
+        nStage.transform.localPosition = pos;
+        stages[0].gameObject = nStage;
+        
+        for (int i = 1; i < stages.Count; ++i)
         {
-            if (lastTransform is null)
-                lastTransform = transform.Find("head").transform;
-
-            Vector3 pos = lastTransform.position - new Vector3(0f, 1f * localScale.y, 0);
-            
-            GameObject nStage = Instantiate(stagePrefab, pos, lastTransform.rotation);
-            nStage.transform.SetParent(transform);
-            nStage.transform.localScale = localScale;
             lastTransform = nStage.transform;
+            pos = lastTransform.localPosition - new Vector3(0f, 4.09f, 0);
+            
+            nStage = Instantiate(stagePrefab, Vector3.zero, lastTransform.rotation);
+            nStage.transform.SetParent(transform);
+            nStage.transform.localPosition = pos;
             stages[i].gameObject = nStage;
         }
         
@@ -136,8 +145,16 @@ public class Rocket : MonoBehaviour
     private void ColliderCentering(int numberOfStages)
     {
         //Capsule set to right place
-        _collider.center = new Vector3(0f, -numberOfStages * 0.5f, 0f);
-        _collider.height = numberOfStages + 1;
+        float height = 2.5f;
+        if (numberOfStages == 1)
+            height += 3.6f;
+        else if (numberOfStages == 2)
+            height += 2f + 5.5f;
+        else
+            height += 2f + (numberOfStages - 2) * 4.25f + 5.5f;
+        
+        _collider.center = new Vector3(0f, 2.5f - height / 2, 0f);
+        _collider.height = height;
     }
 
     public float CurrentHeight()
@@ -159,11 +176,14 @@ public class Rocket : MonoBehaviour
     {
         _isLaunched = true;
         _flameTransform.gameObject.SetActive(true);
+
+        StartCoroutine(nameof(Log));
     }
 
     public void ReSettingRocket(List<Stage> list, float usefulMass)
     {
         if (list.Count == 0) return;
+        StopCoroutine(nameof(Log));
         
         _isLaunched = false;
         DestroyAllStages();
@@ -183,6 +203,16 @@ public class Rocket : MonoBehaviour
         foreach (var stage in stages)
         {
             Destroy(stage.gameObject);
+        }
+    }
+    
+    IEnumerator Log()
+    {
+        _logger.Log(_rigidbody.velocity.y, _rigidbody.mass, height);
+        while(true)
+        {
+            yield return new WaitForFixedUpdate();
+            _logger.LogAppend(_rigidbody.velocity.y, _rigidbody.mass, height);
         }
     }
 
