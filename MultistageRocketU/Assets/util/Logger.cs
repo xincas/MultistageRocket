eae5f9c257a4938b1abfe44df4f5c5a4f286ae2a
xincas <ismrah.25@gmail.com>
2022-07-19 04:18:15 +0300

    Logger class

---
+++ MultistageRocketU/Assets/util/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace util
+{
+    public abstract class LoggerBase
+    {
+        public abstract void LogAppend(float velocity, float mass, float height);
+        public abstract void Log(float velocity, float mass, float height);
+    }
+
+    public class Logger: LoggerBase
+    {
+        private string CurrentDirectory { get; set; }
+        private string FileName { get; set; }
+        private string FilePath { get; set; }
+
+        public Logger()
+        {
+            CurrentDirectory = Directory.GetCurrentDirectory();
+            FileName = "data_u.txt";
+            FilePath = CurrentDirectory + "/" + FileName;
+        }
+        
+        public override void LogAppend(float velocity, float mass, float height)
+        {
+            using (System.IO.StreamWriter w = System.IO.File.AppendText(this.FilePath))
+            {
+                w.WriteLine("{0} {1} {2}", velocity, mass, height);
+            }
+        }
+
+        public override void Log(float velocity, float mass, float height)
+        {
+            using (System.IO.StreamWriter w = System.IO.File.CreateText(this.FilePath))
+            {
+                w.WriteLine("{0} {1} {2}", velocity, mass, height);
+            }
+        }
+    }
+}
